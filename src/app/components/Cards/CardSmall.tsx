772f486b594ab95a1ff2c85d0cb671611edc1fe1
nacukisubaru <alexitsheff@gmail.com>
2022-08-06 14:54:58 +0300

    стили

--- src/app/components/Cards/CardSmall.tsx
+++ src/app/components/Cards/CardSmall.tsx
@@ -13,57 +13,66 @@ const CardSmall: FC<ICardSmall> = ({ id, title, description, price, img, buy, sh
             <Card
                 sx={{
                     width: 300,
-                    height: 380,
+                    height: 390,
                     margin: "10px",
                     borderRadius: "15px",
                     paddingTop: "36px"
                 }}
             >
-                <Box
-                    className="card-img"
-                    style={{ backgroundSize: "contain" }}
-                    sx={{ background: `url(${img}) center center no-repeat` }}
-                ></Box>
-                <Typography
-                    variant="body2"
-                    style={{
-                        fontWeight: "bold",
-                        fontSize: "18px",
-                    }}
-                >
-                    {title}
-                </Typography>
-                <Typography variant="body2" style={{ marginBottom: "10px" }}>
-                    {description}
-                </Typography>
-                <Typography variant="body2" style={{ marginBottom: "25px" }}>
-                    {price}
-                </Typography>
-                <Button
-                    className="card__button"
-                    variant="contained"
-                    style={{ 
-                        width: "279px", 
-                        background: "#896043",
-                        marginBottom: '5px',
-                        height: '30px'
-                    }}
-                    onClick={buy}
-                >
-                    Купить
-                </Button>
-                <Button
-                    className="card__button"
-                    variant="contained"
-                    style={{ 
-                        width: "279px", 
-                        background: "#896043",
-                        height: '30px'
-                    }}
-                    onClick={()=>{show(id)}}
-                >
-                    Детально
-                </Button>
+                <div className="card-content">
+                    <Box
+                        className="card-img"
+                        style={{ backgroundSize: "contain" }}
+                        sx={{ background: `url(${img}) center center no-repeat` }}
+                    ></Box>
+                    <Typography
+                        variant="body2"
+                        style={{
+                            fontWeight: "bold",
+                            fontSize: "18px",
+                        }}
+                    >
+                        {title}
+                    </Typography>
+                    
+                    <div className="card-content-desc">
+                        <Typography variant="body2">
+                            {description}
+                        </Typography>
+                    </div>
+
+                    <div className="card-content-price">
+                        <Typography variant="body2" style={{fontSize: '20px'}}>
+                            {price} &#x20bd;
+                        </Typography>
+                    </div>
+
+                    <Button
+                        className="card__button"
+                        variant="contained"
+                        style={{ 
+                            width: "279px", 
+                            background: "#896043",
+                            marginBottom: '5px',
+                            height: '30px'
+                        }}
+                        onClick={buy}
+                    >
+                        Купить
+                    </Button>
+                    <Button
+                        className="card__button"
+                        variant="contained"
+                        style={{ 
+                            width: "279px", 
+                            background: "#896043",
+                            height: '30px'
+                        }}
+                        onClick={()=>{show(id)}}
+                    >
+                        Детально
+                    </Button>
+                </div>
             </Card>
         </>
     );
