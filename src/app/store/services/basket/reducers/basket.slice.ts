84312fd5a0a35b1efecb88d46e6d08833ec2bab6
nacukisubaru <alexitsheff@gmail.com>
2022-08-09 13:47:28 +0300

    фикс покупок

--- src/app/store/services/basket/reducers/basket.slice.ts
+++ src/app/store/services/basket/reducers/basket.slice.ts
@@ -1,10 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IProductСharacteristics } from "../../../../types/product.types";
 
 const initialState = {
     list:<IProductСharacteristics[]> [],
     count:<number> 0
 };
+
+const findItemInBasket = (basket: IProductСharacteristics[], id: number): IProductСharacteristics => {
+    let basketItem: any = false;
+    const items: IProductСharacteristics[] = basket.filter(item => {
+        if(item.id === id) {
+            return item;
+        }
+    });
+
+    if(items.length > 0) {
+        basketItem = items[0];
+    }
+    return basketItem;
+}
 
 export const basketSlice = createSlice({
     name: 'basket',
@@ -13,7 +27,7 @@ export const basketSlice = createSlice({
         addItem: (state, action) => {
             state.list.push(action.payload);
         },
-        removeItem: (state, action) => {
+        removeItem: (state, action: PayloadAction<{id: number}>) => {
             let itemToRemove: any = {};
             state.list = state.list.filter(item => {
                 if(item.id !== action.payload.id) {
@@ -25,31 +39,26 @@ export const basketSlice = createSlice({
             
             state.count = state.count - 1;
         },
-        plusQuantity: (state, action) => {
+        plusQuantity: (state, action: PayloadAction<{id: number, value: number}>) => {
             const obj = action.payload;
-            console.log(state.list, obj);
-            const items = state.list.filter(item => {
-                if(item.id === obj.id) {
-                    return item;
-                }
-            });
-            
-            items[0].quantity = items[0].quantity + obj.value;
-        },
-        minusQuantity: (state, action) => {
-            if(state.count > 2) {
-                const obj = action.payload;
-                state.list[obj.id].quantity = state.list[obj.id].quantity - obj.value;
+            const item = findItemInBasket(state.list, obj.id);
+            if(item) {
+                item.quantity = item.quantity + obj.value;
             }
         },
-        setQuantity: (state, action) => {
+        minusQuantity: (state, action: PayloadAction<{id: number, value: number}>) => {
             const obj = action.payload;
-            const items = state.list.filter(item => {
-                if(item.id === obj.id) {
-                    return item;
-                }
-            });
-            items[0].quantity = obj.value;
+            const item = findItemInBasket(state.list, obj.id);
+            if(item) {
+                item.quantity = item.quantity - obj.value;
+            }
+        },
+        setQuantity: (state, action: PayloadAction<{id: number, value: number}>) => {
+            const obj = action.payload;
+            const item = findItemInBasket(state.list, obj.id);
+            if(item) {
+                item.quantity = obj.value;
+            }
         },
         plusCountPosition: (state) => {
             state.count = state.count + 1;
