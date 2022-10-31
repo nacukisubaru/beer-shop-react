dc7c7b019b43089ff1808641ff30112758af9668
nacukisubaru <alexitsheff@gmail.com>
2022-10-31 19:47:15 +0300

    передача boolean параметра в фильтрах forBottling и filtered

--- src/app/store/reducers/filter.products.ts
+++ src/app/store/reducers/filter.products.ts
@@ -10,8 +10,8 @@ export const initialState = {
     maxVolume:<number> 0,
     minFortress:<number> 0,
     maxFortress:<number> 0,
-    forBottling:<any> undefined,
-    filtered:<any> undefined,
+    forBottling:<any> false,
+    filtered:<any> false,
     sort: ['price', 'ASC'],
     q: ''
 }
@@ -86,8 +86,8 @@ export const filterProductsSlice = createSlice({
             state.minVolume = 0;
             state.minFortress = 0;
             state.maxFortress = 0;
-            state.forBottling = undefined;
-            state.filtered = undefined;
+            state.forBottling = false;
+            state.filtered = false;
         }
     }
 });
