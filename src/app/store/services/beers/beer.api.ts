1fbe573e21f08f6c40a87d4f37ccf3e033de0194
nacukisubaru <alexitsheff@gmail.com>
2022-11-03 23:58:14 +0300

    добавлям просмотры по новой ссылке

--- src/app/store/services/beers/beer.api.ts
+++ src/app/store/services/beers/beer.api.ts
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { host } from "../../../http/http.request.config";
 
 export const beerApi = createApi({
-    reducerPath: 'beers',
-    baseQuery: fetchBaseQuery({baseUrl: host + '/beers'}),
+    reducerPath: 'products',
+    baseQuery: fetchBaseQuery({baseUrl: host + '/products'}),
     endpoints: (build) => ({
         addShowBeer: build.mutation({
             query: (id: number) => ({
