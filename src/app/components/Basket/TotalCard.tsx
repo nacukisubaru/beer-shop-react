c43d3abe784e35315815399e49c9fdae7fcb92bf
nacukisubaru <alexitsheff@gmail.com>
2022-11-04 14:35:01 +0300

    адрес откуда можно забрать заказ в карточке на странице корзины

--- src/app/components/Basket/TotalCard.tsx
+++ src/app/components/Basket/TotalCard.tsx
@@ -21,6 +21,10 @@ const TotalCard: FC<ITotalCard> = ({ totalPrice, order }) => {
                     <Typography variant="h5">Итого</Typography>
                     <Typography variant="h5">{totalPrice} р</Typography>
                 </div>
+                <div className="total-card-details">
+                    <Typography variant="h6" style={{display: "flex"}}>Доставка:</Typography>
+                    <Typography variant="body2" style={{display: "flex", marginLeft: '-14px'}}>Ежедневно с 12:00 до 22:00 г Калуга, Улица Братьев Луканиных 7</Typography>
+                </div>
                 <Button sx={{ width: 250 }} onClick={order} variant="contained">
                     Заказать
                 </Button>
