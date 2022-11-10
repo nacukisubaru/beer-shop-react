c3633ea0f994035ef0c99193d09541b60b323b66
nacukisubaru <alexitsheff@gmail.com>
2022-11-10 12:36:56 +0300

    скрытие лишних полей для фильтра и отключение лишних способов фильтрации

--- src/app/components/Grid/TableGrid.tsx
+++ src/app/components/Grid/TableGrid.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material";
-import { DataGrid, GridSortItem } from "@mui/x-data-grid";
+import { DataGrid, getGridStringOperators, GridSortItem, GridToolbar } from "@mui/x-data-grid";
 import { FC, useState } from "react";
 import { useActions } from "../../hooks/useActions";
 import PaginationGrid from "./PaginationGrid";
@@ -13,6 +13,9 @@ interface ITableGridProps {
 const TableGrid:FC<ITableGridProps> = ({columns, rows, pageSize}) => {
     const { setContentSort, setContentDefaultSort } = useActions();
     const [sortModel, setSortModel] = useState<GridSortItem[]>([{ field: 'id', sort: 'desc' }]);
+    const filterOperators = getGridStringOperators().filter(({ value }) => 
+        ['contains', 'equals' /* add more over time */ ].includes(value)
+    );
 
     const handleSortChange = (sortArray: any) => {
         if(sortArray.length) {
@@ -28,19 +31,23 @@ const TableGrid:FC<ITableGridProps> = ({columns, rows, pageSize}) => {
 
     return (
         <div style={{ height: 860, width: "100%", marginBottom: "10px"}}>
-            {/* <Button sx={{ width: 259 }} onClick={order} variant="contained">
-                Добавить
-            </Button> */}
+            {/* <div>
+                <Button sx={{ width: 259 }}  variant="contained">
+                    Добавить
+                </Button>
+            </div> */}
             <DataGrid
                 rows={rows}
-                columns={columns}
+                columns={columns.map((item)=>{return {...item, filterOperators}})}
                 pageSize={pageSize}
                 rowsPerPageOptions={[]}
                 onSortModelChange={handleSortChange}
                 checkboxSelection
                 style={{ marginBottom: "40px" }}
                 sortModel={sortModel}
-                components={{Footer: PaginationGrid}}
+                components={{Footer: PaginationGrid, Toolbar: GridToolbar }}
+                onFilterModelChange={()=>{console.log('we cand send request here!')}}
+                localeText={{filterOperatorEquals:'Равняется', filterOperatorContains: 'Содержит в себе'}}
             />
         </div>
     );
--- src/pages/admin/content/products/beer.tsx
+++ src/pages/admin/content/products/beer.tsx
@@ -1,3 +1,4 @@
+import { getGridStringOperators } from "@mui/x-data-grid";
 import AdminPanel from "../../../../app/components/Admin/WorkSpace/AdminPanel";
 import { useAppSelector } from "../../../../app/hooks/useAppSelector";
 import { useCatalog } from "../../../../app/hooks/useCatalog";
@@ -9,21 +10,21 @@ export default function BeerAdmin() {
         { field: "title", headerName: "Название", width: 200 },
         { field: "description", headerName: "Описание", width: 150 },
         { field: "price", headerName: "Цена", width: 150 },
-        { field: "quantity", headerName: "Количество", width: 150 },
+        { field: "quantity", headerName: "Количество", width: 150, filterable: false },
         { field: "isActive", headerName: "Активность", width: 150 },
         { field: "inStock", headerName: "В наличии", width: 150 },
         { field: "brandName", headerName: "Название бренда", width: 150 },
         { field: "typePackagingName", headerName: "Тип упаковки", width: 150 },
-        { field: "compound", headerName: "Состав", width: 150 },
-        { field: "volume", headerName: "Объем", width: 150 },
-        { field: "fortress", headerName: "Крепкость", width: 150 },
-        { field: "ibu", headerName: "ibu", width: 150 },
+        { field: "compound", headerName: "Состав", width: 150, filterable: false },
+        { field: "volume", headerName: "Объем", width: 150, filterable: false },
+        { field: "fortress", headerName: "Крепкость", width: 150, filterable: false },
+        { field: "ibu", headerName: "ibu", width: 150, filterable: false },
         { field: "forBottling", headerName: "На розлив", width: 150 },
         { field: "filtred", headerName: "Фильтрованное", width: 150 },
     ];
     const {rows} = useCatalog(beerApi, 'beer');
     const {limitPage} = useAppSelector(state => state.contentReducer);
-
+  
     return (
         <>
             <AdminPanel
