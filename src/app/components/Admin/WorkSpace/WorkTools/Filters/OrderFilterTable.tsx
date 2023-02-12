e8d30d9127b3b898e5d432e95367020b261e2a75
nacukisubaru <incloudff@gmail.com>
2023-02-12 22:23:36 +0300

    фильтр по статусу в таблице заказы

--- src/app/components/Admin/WorkSpace/WorkTools/Filters/OrderFilterTable.tsx
+++ src/app/components/Admin/WorkSpace/WorkTools/Filters/OrderFilterTable.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
 import { useActions } from "../../../../../hooks/useActions";
 import { useAppSelector } from "../../../../../hooks/useAppSelector";
+import { orderStatusApi } from "../../../../../store/services/order/order.api";
+import { IOrderStatus } from "../../../../../store/services/order/types/order.types";
 import FilterPanelGrid from "../../../../Grid/FilterPanelGrid";
 
 const OrderFilterTable: FC = () => {
     const { setFilter, removeFilter, resetFilters, filter} = useActions();
     const { tmpfilters } = useAppSelector(state => state.contentReducer);
+    const statusList = orderStatusApi.useGetListQuery({});
 
     const handleSetFilter = (name: string, value: number | string | number[] | string[]) => {
         setFilter({name, value});
@@ -22,6 +25,17 @@ const OrderFilterTable: FC = () => {
                 { field: "orderId", fieldName: "ID заказа", inputNumber: true },
                 { field: "customerFio", fieldName: "ФИО покупателя", inputText: true },
                 { field: "customerPhone", fieldName: "Телефон покупателя", inputText: true },
+                {
+                    field: "statusId",
+                    fieldName: "Статус",
+                    inputSelect: {
+                        valueInputSelect: statusList.data
+                            ? statusList.data.map((item: IOrderStatus) => {
+                                  return { name: item.statusName, value: item.id };
+                              })
+                            : [],
+                    },
+                },
             ]}
             filters={tmpfilters}
             setCustomFilter={handleSetFilter}
