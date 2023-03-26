10c98eae34279329dcdede663d2e8ec5dfd7adcf
nacukisubaru <incloudff@gmail.com>
2023-03-26 17:29:13 +0300

    fix

--- src/app/http/http.request.config.ts
+++ src/app/http/http.request.config.ts
@@ -3,7 +3,6 @@ export let hostAdminPanel = 'http://localhost:1337';
 export const limitPage = 10;
 export const limitPageAdmin = 12;
 
-// if(process.env.NODE_ENV === 'production') {
-//     host = 'http://45.8.251.222:5000';
-//     hostAdminPanel = 'http://45.8.251.222:1337';
-// }
+if(process.env.NODE_ENV === 'production') {
+    host = 'https://beer-grad.online';
+}
